175213179a26a7977c1f5bb513b07a8c151eceab
Alexander Malmberg <alexnostahp@gmail.com>
2019-08-07 18:03:11 +0200

    added more media queries

--- src/components/pages/employees.js
+++ src/components/pages/employees.js
@@ -5,32 +5,25 @@ import React from 'react';
 function Employees() {
   return (
 
-    <div className="news-page">
-        
-        <div className="products-page">
+    
+    
+      <div className="products-page">
 
-      <div className="product-item">
-        <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
-        <p>Namm: Peter</p>
-      </div>
+        <div className="product-item">
+          <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
+          <p>Namm: Peter</p>
+        </div>
 
-      <div className="product-item">
-        <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
-        <p>Namm: Jimmy</p>
-      </div>
+        <div className="product-item">
+          <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
+          <p>Namm: Jimmy</p>
+        </div>
 
-      <div className="product-item">
-        <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
-        <p>Namm: Lennart</p>
-      </div>
+        <div className="product-item">
+          <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
+          <p>Namm: Lennart</p>
+        </div>
       
-
-      
-
-    </div>
-
-       
-
     </div>
 
   );
