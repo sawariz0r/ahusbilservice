3d26a2ff4f2874379da679bc99ab99e09f774f8e
Alexander Malmberg <alexnostahp@gmail.com>
2019-08-07 23:32:57 +0200

    media queries changes and refining

--- src/components/pages/employees.js
+++ src/components/pages/employees.js
@@ -7,19 +7,19 @@ function Employees() {
 
     
     
-      <div className="products-page">
+      <div className="employees-page">
 
-        <div className="product-item">
+        <div className="employees-item">
           <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
           <p>Namm: Peter</p>
         </div>
 
-        <div className="product-item">
+        <div className="employees-item">
           <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
           <p>Namm: Jimmy</p>
         </div>
 
-        <div className="product-item">
+        <div className="employees-item">
           <img src="https://i2.wp.com/getrake.io/wp-content/uploads/2017/10/missing-profile-photo.png?fit=256%2C256&ssl=1" alt="error loading"></img>
           <p>Namm: Lennart</p>
         </div>
